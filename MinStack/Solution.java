bac9103d5d00cf8da54205ca5af92b70a584bc9f
qinshulei <527072230@qq.com>
2015-05-21 01:03:35 +0800

    add MinStack solution

--- MinStack/Solution.java
+++ MinStack/Solution.java
@@ -1,83 +1,63 @@
 /*
-https://leetcode.com/problems/min-stack/
+  https://leetcode.com/problems/min-stack/
 
-Min Stack Total Accepted: 29916 Total Submissions: 166911 My Submissions Question Solution
-Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+  Min Stack Total Accepted: 29916 Total Submissions: 166911 My Submissions Question Solution
+  Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
 
-push(x) -- Push element x onto stack.
-pop() -- Removes the element on top of the stack.
-top() -- Get the top element.
-getMin() -- Retrieve the minimum element in the stack.
+  push(x) -- Push element x onto stack.
+  pop() -- Removes the element on top of the stack.
+  top() -- Get the top element.
+  getMin() -- Retrieve the minimum element in the stack.
+
+  ***MARK***
+  没做出来，之前做的超时。主要问题是，我尝试实现一个保存顺序的stack。但使用链表作为内部存储，
+  导致push性能过差，最终结果超时。但下面这位同学使用两个stack保存就很有创意了。
+  类似于以前找最小值的程序，如今存了所有最小值的历史，自然使得最小值这一属性和stack默认属性也一样了。
+
+  Min Stack My Submissions Question Solution
+
+  Hints:
+Consider space-time tradeoff. How would you keep track of the minimums using extra space?
+Make sure to consider duplicate elements.
+O(n) runtime, O(n) space – Extra stack:
+
+Use an extra stack to keep track of the current minimum value. During the push operation we choose the new element or the current minimum, whichever that is smaller to push onto the min stack.
+
+O(n) runtime, O(n) space – Minor space optimization:
+
+If a new element is larger than the current minimum, we do not need to push it on to the min stack. When we perform the pop operation, check if the popped element is the same as the current minimum. If it is, pop it off the min stack too.
 
 */
 
 import java.util.*;
 
 public class Solution {
-    class Node{
-        public int data;
-        public Node next;
-        public Node pre;
-        public Node(int data, Node pre, Node next){
-          this.next = next;
-          this.data = data;
-          this.pre = pre;
-       }
-    }
-    private Node[] mStackArray = new Node[10];
-    private int top;
-    private Node head = new Node(0, null, null);
+
+    private Stack<Integer> stack = new Stack<Integer>();
+    private Stack<Integer> minStack = new Stack<Integer>();
+
     public void push(int x) {
-        Node node = new Node(x, null,null);
-        if(mStackArray.length == top + 1)
-        {
-            Node[] tempArray = new Node[mStackArray.length*2];
-            System.arraycopy(tempArray, 0, mStackArray, 0, mStackArray.length);
-            mStackArray = tempArray;
-        }
-        mStackArray[++top] = node;
-        if (head.next == null){
-            head.next = node;
-            head.pre = node;
-            node.next = head;
-            node.pre = head;
-        } else {
-            Node cur = head.next;
-            while(!cur.equals(head) && cur.next.data <= x)
-            {
-                cur=cur.next;
-            }
-            cur.pre.next = node;
-            node.next = cur;
-            node.pre = cur.pre;
-            cur.pre = node;
+        stack.push(x);
+        if (minStack.isEmpty() || x <= minStack.peek()) {
+            minStack.push(x);
         }
     }
 
     public void pop() {
-        if (top == 0){
-            head.next = null;
-            head.pre = null;
-            mStackArray[top] = null;
-            top = -1;
-            return;
+        if (!stack.isEmpty()) {
+            int x = stack.pop();
+            if (x == minStack.peek()) {
+                minStack.pop();
+            }
         }
-
-        Node node = mStackArray[top];
-        Node pre = node.pre;
-        Node next = node.next;
-        pre.next = next;
-        next.pre = pre;
-        mStackArray[top] = null;
-        top = top - 1;
     }
 
     public int top() {
-        return mStackArray[top].data;
+        return stack.peek();
     }
 
     public int getMin() {
-        return head.next.data;
+        return minStack.peek();
     }
 
     public static void main(String[] args){
