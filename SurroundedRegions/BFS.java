71d5fe9d79ef375a47f582e1981afe8046f481cf
qinshulei <527072230@qq.com>
2015-05-22 00:36:18 +0800

    add bfs

--- SurroundedRegions/BFS.java
+++ SurroundedRegions/BFS.java
@@ -1,28 +1,28 @@
 /*
-https://leetcode.com/problems/surrounded-regions/
+  https://leetcode.com/problems/surrounded-regions/
 
-Surrounded Regions Total Accepted: 29284 Total Submissions: 200303 My Submissions Question Solution
-Given a 2D board containing 'X' and 'O', capture all regions surrounded by 'X'.
+  Surrounded Regions Total Accepted: 29284 Total Submissions: 200303 My Submissions Question Solution
+  Given a 2D board containing 'X' and 'O', capture all regions surrounded by 'X'.
 
-A region is captured by flipping all 'O's into 'X's in that surrounded region.
+  A region is captured by flipping all 'O's into 'X's in that surrounded region.
 
-For example,
-X X X X
-X O O X
-X X O X
-X O X X
-After running your function, the board should be:
+  For example,
+  X X X X
+  X O O X
+  X X O X
+  X O X X
+  After running your function, the board should be:
 
-X X X X
-X X X X
-X X X X
-X O X X
+  X X X X
+  X X X X
+  X X X X
+  X O X X
 
 */
 
 import java.util.*;
 
-public class BFS {
+public class Solution {
     class Node
     {
         public int x;
@@ -33,68 +33,42 @@ public class BFS {
             this.y = y;
         }
     }
-    private Queue<Node> queue = newLinkedList<Node>();
-    private vode bfs(char[][] board,int x,int y)
+    private Queue<Node> queue = new LinkedList<Node>();
+    private void bfs(char[][] board)
     {
-        while( !queue.empty() )
-        {
-            Node node = queue.poll();
-            if( node.x >=0 && node.x <=          {
-                board[r][c] = '#';
-                q.push( make_pair( r - 1, c) );
-                q.push( make_pair( r + 1, c) );
-                q.push( make_pair( r, c - 1) );
-                q.push( make_pair( r, c + 1) );
+        while( !queue.isEmpty() )
+            {
+                Node node = queue.poll();
+                if(node.x >=0 && node.x <= board.length-1 && node.y>=0 && node.y<=board[0].length-1 && board[node.x][node.y] =='O' )                    {
+                    board[node.x][node.y] = '.';
+                    queue.add( new Node(node.x+1,node.y) );
+                    queue.add( new Node(node.x-1,node.y) );
+                    queue.add( new Node(node.x,node.y+1) );
+                    queue.add( new Node(node.x,node.y-1) );
+                }
             }
-        }
     }
     public void solve(char[][] board) {
-        visit = new boolean[board.length][board[0].length];
-        temp = new boolean[board.length][board[0].length];
-
-        for (int row = 0; row < borad.length; row ++) {
+        if(board.length < 3)
+            return;
+        for (int row = 0; row < board.length; row ++) {
             for (int col = 0; col < board[row].length; col ++) {
-                if ( board[row][col] != '.' && ( row == 0 || col == 0 ) && board[row][col] == 'O') {
-                    dfs(board, row, col);
+                if ( ( row == 0 || col == 0 || row == board.length-1 || col == board[row].length-1) && board[row][col] == 'O') {
+                    queue.add(new Node(row, col));
+                    //  board[row][col] ='.';
                 }
             }
         }
-    }
-
-    public void dfs(char[][] board,int row, int col){
-
-        if ( row - 1 >= 0 && board[row - 1][col] == 'O'){
-            board[row - 1][col] = '.';
-            dfs(board, row - 1, col);
-        }
-
-        if ( col + 1 < board[row].length && board[row][col + 1] == 'O'){
-            board[row][col + 1] = '.';
-            dfs(board, row, col + 1);
-        }
-
-        if ( row + 1 < board.length && board[row + 1][col] == 'O'){
-            board[row + 1][col] = '.';
-            dfs(board, row + 1, col);
-        }
-
-        if ( col - 1 >= 0 && board[row][col - 1] == 'O'){
-            board[row][col - 1] = '.';
-            dfs(board, row, col - 1);
-        }
-    }
-
-    public void xx(char[][] board){
-        for (int row = 0; row < borad.length; row ++) {
+        bfs(board);
+        for (int row = 0; row < board.length; row ++) {
             for (int col = 0; col < board[row].length; col ++) {
-                if ( board[row][col] == '.' ) {
-
+                if ( board[row][col] == 'O') {
+                    board[row][col] = 'X';
+                }
+                else if(board[row][col] == '.'){
+                    board[row][col] = 'O';
                 }
             }
         }
-    }
-
-    public static void main(String[] args){
-
     }
 }
