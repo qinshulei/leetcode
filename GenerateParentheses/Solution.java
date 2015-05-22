39766797d25d6c023ea210dfdb50517452d04bf6
qinshulei <527072230@qq.com>
2015-05-22 23:18:42 +0800

    add problem generate parentheses

--- GenerateParentheses/Solution.java
+++ GenerateParentheses/Solution.java
@@ -7,23 +7,55 @@
   "((()))", "(()())", "(())()", "()(())", "()()()"
 
 */
+import java.util.*;
 
 public class Solution {
-    private List<String> result = new ArrayList<String>();
-    private int size = 0;
+    public static class Parenthes {
+        public List<Parenthes> inner = new ArrayList<Parenthes>();
+        public String toString(){
+            StringBuilder builder = new StringBuilder();
+            builder.append("(");
+            for (int i = 0; i < inner.size(); i++){
+                builder.append(inner.get(i).toString());
+            }
+            builder.append(")");
+            return builder.toString();
+        }
+    }
+
     public List<String> generateParenthesis(int n) {
-        size = n;
-        dfs("(", 1, 0);
+        Parenthes parent = new Parenthes();
+        Set<String> ret = new TreeSet<String>();
+        List<Parenthes> save = new ArrayList<Parenthes>();
+        save.add(parent);
+        solve(parent,n,save,ret);
+        List<String> result = new ArrayList<String>(ret);
         return result;
     }
-    public void dfs(String s, int left, int right){
-        if(left > size || right > size || right > left)
-            return;
-        if(left==right && left==size){
-            result.add(s);
+
+    public void solve(Parenthes p, int n,List<Parenthes> save,Set<String> ret){
+        if (n == 0){
+            String s = p.toString();
+            ret.add(s.substring(1,s.length()-1));
             return;
         }
-        dfs(s+"(",left+1, right);
-        dfs(s+")",left, right+1);
+
+        for (int i = save.size() - 1; i >= 0;i--){
+            Parenthes node = save.get(i);
+            Parenthes item = new Parenthes();
+            node.inner.add(item);
+            save.add(item);
+            solve(p,n - 1,save,ret);
+            node.inner.remove(item);
+            save.remove(item);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        List<String> ret = s.generateParenthesis(3);
+        for(int i = 0; i < ret.size(); i++){
+            System.out.println(ret.get(i));
+        }
     }
 }
