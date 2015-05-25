0ab6f316ff456ffa43a392b88c6f84131a58abb8
qinshulei <527072230@qq.com>
2015-05-26 00:06:07 +0800

    update

--- ImplementStrStr/Solution.java
+++ ImplementStrStr/Solution.java
@@ -6,6 +6,12 @@ Implement strStr().
 
 Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
+这题一开始，我并没有想到用 haystack.length()-needle.length() 的方式去保证，下面的匹配不会越界，
+反而是直接和以前一样遍历整个haystack.这也说明由于自己习惯了写一个传统的循环，而忘记正在的去思考循环的目的。
+这里循环是为了循环可用的起点。因此明显haystack结尾部分可能并不是一个值得遍历的位置。
+写for循环时，我们应该充分思考循环的每一个条件，而不是机械的写一个循环模板。
+充分思考每一行代码的目的，以及它最该在什么位置出现，可以在什么时候出现，并考虑代码可读性去则中。
+就和面向对象中的类方法一样。思考每一个方法是谁的职责，它该在什么位置。
 */
 
 import java.util.*;
@@ -16,11 +22,9 @@ public class Solution {
         if ("".equals(haystack) && "".equals(needle)){
             return 0;
         }
-        outer :
-        for (int i = 0; i < haystack.length(); i ++) {
+        outer : for (int i = 0; i <= haystack.length()-needle.length(); i++) {
             for (int j = 0; j < needle.length(); j ++) {
-                if ( (j == 0 && i + needle.length() - 1 >= haystack.length())
-                     || haystack.charAt(i + j) != needle.charAt(j)) {
+                if ( haystack.charAt(i + j) != needle.charAt(j)) {
                     continue outer;
                 }
             }
