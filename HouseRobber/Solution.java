bb28728862f31eac9fd34592e9bd6c4699f6dcce
qinshulei <527072230@qq.com>
2015-05-26 22:41:13 +0800

    add cache to solve time limited

--- HouseRobber/Solution.java
+++ HouseRobber/Solution.java
@@ -20,15 +20,26 @@ public class Solution {
 
     public int rob(int[] nums) {
         cache = new int[nums.length];
-        return solve(nums,0);
+        for (int i = 0; i < cache.length; i++) {
+            cache[i] = -1;
+        }
+        return solve(nums,0,0);
     }
 
-    public int solve(int[] nums, int cur) {
+    public int solve(int[] nums, int cur,int unRobTimes) {
         if (cur >= nums.length){
             return 0;
         }
-        int robCur = nums[cur] + solve(nums, cur + 2);
-        int unRobCur = solve(nums, cur + 1);
+        if (cache[cur] != -1 ){
+            return cache[cur];
+        }
+
+        if (unRobTimes >= 2){
+            return nums[cur] + solve(nums, cur + 2, 0);
+        }
+
+        int robCur = nums[cur] + solve(nums, cur + 2, 0);
+        int unRobCur = solve(nums, cur + 1, unRobTimes + 1);
 
         int max = Math.max(robCur,unRobCur);
         cache[cur] = max;
@@ -43,6 +54,7 @@ public class Solution {
         System.out.println(s.rob(test));
 
         int[] test2 = {104,209,137,52,158,67,213,86,141,110,151,127,238,147,169,138,240,185,246,225,147,203,83,83,131,227,54,78,165,180,214,151,111,161,233,147,124,143};
+        int[] test3 = {114,117,207,117,235,82,90,67,143,146,53,108,200,91,80,223,58,170,110,236,81,90,222,160,165,195,187,199,114,235,197,187,69,129,64,214,228,78,188,67,205,94,205,169,241,202,144,240};
         System.out.println(s.rob(test2));
     }
 }
