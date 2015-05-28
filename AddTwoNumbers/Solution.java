6c7e874f0521e0066895969ba01ca2dea99aaaf0
qinshulei <527072230@qq.com>
2015-05-28 23:22:48 +0800

    update

--- AddTwoNumbers/Solution.java
+++ AddTwoNumbers/Solution.java
@@ -7,6 +7,7 @@
   Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
   Output: 7 -> 0 -> 8
 
+  使用三目运算符减少if的数量，提高代码的简练程度。
 */
 
 import java.util.*;
@@ -23,8 +24,10 @@ public class Solution {
         ListNode head = null;
         ListNode result = null;
         boolean flag = false;
-        while (l1 != null && l2 != null) {
-            int sum = l1.val + l2.val;
+        while (l1 != null || l2 != null) {
+            int val1 = (l1 == null ? 0 : l1.val);
+            int val2 = (l2 == null ? 0 : l2.val);
+            int sum = val1 + val2;
             if (flag) {
                 sum += 1;
                 flag = false;
@@ -40,36 +43,12 @@ public class Solution {
                 result.next = new ListNode(sum);
                 result = result.next;
             }
-            l1=l1.next;
-            l2=l2.next;
+            l1 = (l1 == null ? null : l1.next);
+            l2 = (l2 == null ? null : l2.next);
         }
-        ListNode elseList = l1;
-        if(l1 == null) {
-            elseList = l2;
+        if(flag) {
+            result.next = new ListNode(1);
         }
-        while (elseList != null) {
-            int sum = elseList.val;
-            if (flag) {
-                sum += 1;
-                flag = false;
-            }
-            if (sum >= 10) {
-                flag = true;
-                sum = sum - 10;
-            }
-            if (result == null) {
-                result = new ListNode(sum);
-                head = result;
-            } else {
-                result.next = new ListNode(sum);
-                result = result.next;
-            }
-            elseList = elseList.next;
-        }
-        if(flag)
-            {
-                result.next = new ListNode(1);
-            }
         return head;
     }
 
