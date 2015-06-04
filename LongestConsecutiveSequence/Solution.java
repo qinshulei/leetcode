ac84c969e63636521ebccd2713926ca1a571a6d0
qinshulei <527072230@qq.com>
2015-06-04 23:47:08 +0800

    add more test

--- LongestConsecutiveSequence/Solution.java
+++ LongestConsecutiveSequence/Solution.java
@@ -96,5 +96,6 @@ public class Solution {
     public static void main(String[] args){
         Solution s = new Solution();
         System.out.println(s.longestConsecutive(new int[]{100, 4, 200, 1, 3, 2}));
+        System.out.println(s.longestConsecutive(new int[]{0}));
     }
 }
