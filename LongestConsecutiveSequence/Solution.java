4926b6e6cc0f3c580bd06b649d41de2eb094effe
qinshulei <527072230@qq.com>
2015-06-05 00:26:57 +0800

    update

--- LongestConsecutiveSequence/Solution.java
+++ LongestConsecutiveSequence/Solution.java
@@ -10,6 +10,15 @@ Given [100, 4, 200, 1, 3, 2],
 The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
 
 Your algorithm should run in O(n) complexity.
+
+这题的关键在与 O(n),意味着只循环一次，那肯定排不了序。我一开始还是想歪了，搞了个bitset，来实现O(n)的排序。但是
+这样使用的内存就超了。
+
+只好看大神答案，我靠，怎么就没想到用hashmap呢。hashmap有效的理想情况实现了O(1)查找。之前之所以没往这个方向想。
+总是觉得O(n)情况下查找太费时。有了hashmap，然后就是遍历整个数组，没来一个数字，更新最长列的状态。
+
+因此发现这里还是一个递推的过程。以后做任何题目先按递推的方式去考虑，从0到1,从1到n，然后考虑保存哪些数据可以递推下去。
+尤其这种O(n)的题目，它必然在每一次循环都要充分利用已有的中间数据。所以关键是如何存和存哪些以前计算出来的中间数据。
 */
 
 import java.util.*;
@@ -17,76 +26,31 @@ import java.util.*;
 public class Solution {
 
     public int longestConsecutive(int[] nums) {
-        BitSet positives = new BitSet(Integer.MAX_VALUE);
-        BitSet negatives = new BitSet(Integer.MAX_VALUE);
-        boolean haveZero = false;
-        boolean haveMin = false;
-        for (int i = 0; i < nums.length; i++){
-            if (nums[i] > 0){
-                positives.set(nums[i] - 1, true);
-            }else if (nums[i] == 0){
-                haveZero = true;
-            } else if (nums[i] == Integer.MIN_VALUE){
-                haveMin = true;
-            } else {
-                negatives.set((-nums[i]) - 1, true);
-            }
-        }
-
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         int max = 0;
-        int startP = 1;
-        int startN = 1;
-        int endP = Integer.MAX_VALUE;
-        int endN = Integer.MAX_VALUE;
-        if (haveZero) {
-            int sum = 1;
-            while (startP <= endP && positives.get(startP - 1)) {
-                startP ++;
-                sum ++;
+        for (int i = 0; i < nums.length; i ++){
+            int current = nums[i];
+            if (map.containsKey(current)){
+                continue;
             }
-            while (startN <= endN && negatives.get(startN - 1)) {
-                startN ++;
-                sum ++;
-            }
-            if (sum > max){
-                max = sum;
-            }
-        }
 
-        if (haveMin){
-            int sum = 1;
-            while(endN >= startN && negatives.get(endN - 1)){
-                endN --;
-                sum ++;
-            }
-            if (sum > max) {
-                max = sum;
-            }
-        }
+            map.put(current, 1);
 
-        for (int i = 0; i < nums.length; i++){
-            if (nums[i] > 0
-                && ( nums[i] == 1 || (nums[i] >= startP && nums[i] <= endP && ! positives.get(nums[i] - 2)) )){
-                int index = startP;
-                int sum = 0;
-                while (index <= endP && positives.get(index - 1)) {
-                    index ++;
-                    sum ++;
-                }
-                if (sum > max) {
-                    max = sum;
-                }
-            } else if( nums[i] < 0
-                       && (nums[i] == -1 || (nums[i] >= startN && nums[i] <= endN && ! negatives.get(nums[i] - 2))) ){
-                int index = startN;
-                int sum = 0;
-                while (index <= endN && negatives.get(index - 1)) {
-                    index ++;
-                    sum ++;
-                }
-                if (sum > max) {
-                    max = sum;
-                }
+            int begin = current;
+            int end = current;
+            if (map.containsKey(begin - 1)){
+                begin = current - map.get(begin - 1);
+            }
+
+            if (map.containsKey(end + 1)){
+                end = current + map.get(end + 1);
+            }
+            int len = end - begin + 1;
+            map.put(begin, len);
+            map.put(end, len);
+
+            if (len > max){
+                max = len;
             }
         }
 
