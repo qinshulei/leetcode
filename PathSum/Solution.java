af09b99af353bf0df38aa645e7235040f52d37e9
qinshulei <527072230@qq.com>
2015-06-12 01:05:21 +0800

    update

--- PathSum/Solution.java
+++ PathSum/Solution.java
@@ -16,6 +16,7 @@
   7    2      1
   return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
 
+  不要熬夜，坑自己。头脑不清，各种莫名其妙的错误。
 */
 
 import java.util.*;
@@ -34,16 +35,64 @@ public class Solution {
 
 
     public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null){
+            return false;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        Stack<Integer> sumStack = new Stack<Integer>();
+        stack.push(root);
+        sumStack.push(sum);
 
-        Stack<Node> stack = new Stack<Node>();
-        Stack<Integer> sum = new Stack<Node>();
+        while(!stack.empty()) {
+            TreeNode cur = stack.pop();
+            int curSum = sumStack.pop();
+            int left = curSum - cur.val;
+            if (cur.left == null && cur.right == null) {
+                if (left == 0) {
+                    return true;
+                }
+            }
 
+            if (cur.right!=null){
+                stack.push(cur.right);
+                sumStack.push(left);
+            }
 
+            if (cur.left!=null) {
+                stack.push(cur.left);
+                sumStack.push(left);
+            }
+        }
 
         return false;
     }
 
     public static void main(String[] args){
+        Solution s = new Solution();
+
+        TreeNode a = new TreeNode(1);
+        TreeNode b = new TreeNode(2);
+        TreeNode c = new TreeNode(3);
+        TreeNode d = new TreeNode(1);
+        TreeNode e = new TreeNode(2);
+        TreeNode f = new TreeNode(3);
+        TreeNode g = new TreeNode(4);
+
+        a.left = b;
+        a.right = c;
+        c.left = d;
+        d.left = e;
+        d.right = f;
+        d.left = g;
+
+        System.out.println("true : " + s.hasPathSum(a, 9));
+
+
+        TreeNode aa = new TreeNode(1);
+        TreeNode bb = new TreeNode(2);
+        aa.left = bb;
+
+        System.out.println("false : " + s.hasPathSum(aa, 0));
 
     }
 }
