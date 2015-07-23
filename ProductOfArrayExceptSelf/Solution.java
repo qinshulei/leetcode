86b42726591ac8fb2c7abc8f207f56503069556a
qinshulei <527072230@qq.com>
2015-07-23 11:07:08 +0800

    rewrite ProductOfArrayExceptSelf use O(n) algorithm

--- ProductOfArrayExceptSelf/Solution.java
+++ ProductOfArrayExceptSelf/Solution.java
@@ -11,7 +11,9 @@ For example, given [1,2,3,4], return [24,12,8,6].
 
 Follow up:
 Could you solve it with constant space complexity? (Note: The output array does not count as extra space for the purpose of space complexity analysis.)
-
+第一次写实在想不出如何复用前面乘法的结果。结果写了O(n^2)的，居然还过了。
+看了讨论，我距离他人的实现其实差关键性的一步，那就是每由想到同时计算正序和逆序两个方向的累积值。
+题目要求的一遍徧历，因此能做的变化，无非徧历的次序。以及缓存。
 */
 
 import java.util.*;
@@ -20,13 +22,18 @@ public class Solution {
 
     public int[] productExceptSelf(int[] nums) {
         int[] output = new int[nums.length];
-        int sum = 1;
+
+        int left = 1;
         for (int i = 0; i < nums.length; i++) {
-            output[i] = sum;
-            sum = sum * nums[i];
-            for (int j = 0; j < i; j++) {
-                output[j] *= nums[i];
-            }
+            output[i] = left;
+            left = left * nums[i];
+
+        }
+
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            output[i] *= right;
+            right = right * nums[i];
         }
 
         return output;
